47f61ef6cc409e70146fe66f796d6a3453375e36
rhythm191 <sac2ndg+develop@gmail>
2020-02-06 16:20:01 +0900

    feat: add stylesheet syntax

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const alfy = require('alfy');
 const expand = require('emmet').default;
 
+const outputs = [];
 const source = alfy.input;
 
 try {
   const title = expand(source, { options: { 'output.format': false }});
   const text = expand(source);
-  alfy.output([{
+  outputs.push({
     title: title,
     arg: text
-  }]);
-} catch {
-  console.log(`error text: ${source}`);
+  });
+} catch(e) {
+  //console.log(`error text: ${source}`);
 }
+
+
+try {
+  const title = expand(source, { type: 'stylesheet', syntax: 'css', options: { 'output.format': false }});
+  const text = expand(source, { type: 'stylesheet', syntax: 'css' });
+  //console.log(title);
+  outputs.push({
+    title: title,
+    arg: text
+  });
+} catch(e) {
+  //console.log(`error style: ${source}`);
+}
+
+alfy.output(outputs);
